d3c976d89c3832767a8ad701246e84a6e39a69a5
HyruleA <l20121017@morelia.tecnm.mx>
2024-10-16 22:05:29 -0600

    Oauth2 JSB

---
+++ src/main/java/dev/smars/oauth2_TSTWM/Application.java
@@ -0,0 +1,13 @@
+package dev.smars.oauth2_TSTWM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/smars/oauth2_TSTWM/Oauth2TstwmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.smars.oauth2_TSTWM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2TstwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
